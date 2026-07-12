142e93f4c85378e42137ab9032255e0e98111fe0
harhyd07-oss <harhyd07@gmail.com>
2026-07-12 20:14:31 +0530

    feat: add styled student cards with hover effects

--- frontend/src/components/StudentCard.jsx
+++ frontend/src/components/StudentCard.jsx
@@ -1,8 +1,23 @@
+import "./StudentCard.css"
 function StudentCard({student}){
     return(
-        <div>
-        {student.name} - {student.roll_number}
+     
+        <div className="card">
+        <h2><strong>{student.name}</strong></h2>
+        <p><strong>Roll Number : </strong>{student.roll_number}</p>
+        <p><strong>Email : </strong>{student.email}</p>
+        <p><strong>Phone : </strong>{student.phone}</p>
+        <p><strong>Department : </strong>{student.department}</p>
+        <p><strong>Year : </strong>{student.year}</p>
+        <p><strong>CGPA : </strong>{student.cgpa}</p>
+        <p><strong>Attendance : </strong>{student.attendance}</p>
+        
+        <div className="button-container">
+            <button>Edit</button>
+            <button>Delete</button>
+            </div>    
         </div>
+        
     )
 }
 
